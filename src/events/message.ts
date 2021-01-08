57dc1677e8d3e59b2585627844ae21bc31222b5b
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-08 20:26:16 +0100

    developer commands

--- src/events/message.ts
+++ src/events/message.ts
@@ -2,13 +2,16 @@ import { Message } from 'discord.js'
 import { Command, Luke } from '../index'
 import EmbedHandler from '../handlers/embed'
 
-const { prefix } = require('../../files/config.json')
+const { prefix, developers } = require('../../files/config.json')
 
 export const _ = async(message: Message, Luke: Luke) => {
   const [commandName, ...args] = message.content.slice(prefix.length).split(/ +/g)
   const command: Command = await Luke.commandHandler.get(commandName)
   if (command) {
     const output = await command.data.execute(message, ...args)
+    
+    if (command.data.dev && !developers.includes(message.author.id)) return
+
     EmbedHandler(output, message, Luke)
   }
 }
