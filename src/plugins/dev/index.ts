3639834f5e58e088f7fbd4981231f56aa4ce7923
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-01-08 20:29:32 +0100

    unnecessary code

--- src/plugins/dev/index.ts
+++ src/plugins/dev/index.ts
@@ -1,7 +1,6 @@
 // https://github.com/badosz0/badosz-bot
 const commands = require('fs')
   .readdirSync(`${__dirname}/commands/`)
-  .filter((file: string) => file !== 'index.js')
   .map((file: string) => require(`${__dirname}/commands/${file}`))
 
 export const data = {
