ce487255516a0ed2a49666124f199ae5154a279d
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-11 12:30:31 +0100

    ban by id

--- src/plugins/moderation/commands/ban.ts
+++ src/plugins/moderation/commands/ban.ts
@@ -1,3 +1,4 @@
+import Luke from 'index'
 import { Command } from '../../../types'
 
 const config = require('../../../../files/config.json')
@@ -14,23 +15,39 @@ const command: Command = {
         const member = message.mentions.members?.first()
         const reason = args.join(' ').replace(args[0], '').replace(' ', '')
         
-        if (!member) return undefined
-        if (!member.bannable) return {
-            title: ':x: Error.',
-            description: 'You can\'t ban this member.',
-            color: config.colors.error
+        if (!member) {
+            const user = await Luke.users.fetch(args[0])
+            if (user) {
+                message.guild?.members.ban(user)
+                return {
+                    title: ':hammer: Member has been banned.',
+                    fields: [
+                        ['Moderator', `${message.author.tag} (${message.author.id})`, true],
+                        ['Member', `${user.tag} (${user.id})`, true],
+                        ['Reason', reason || 'none']
+                    ],
+                    color: config.colors.done
+                }
+            }
         }
+        else {
+            if (!member.bannable) return {
+                title: ':x: Error.',
+                description: 'You can\'t ban this member.',
+                color: config.colors.error
+            }
 
-        reason ? member.ban({ reason: `${message.author.tag} (${message.author.id}): ${reason}`, days: 7 }) : member.ban()
-        
-        return {
-            title: ':hammer: Member has been banned.',
-            fields: [
-                ['Moderator', `${message.author.tag} (${message.author.id})`, true],
-                ['Member', `${member.user.tag} (${member.user.id})`, true],
-                ['Reason', reason || 'none']
-            ],
-            color: config.colors.done
+            reason ? member.ban({ reason: `${message.author.tag} (${message.author.id}): ${reason}`, days: 7 }) : member.ban()
+            
+            return {
+                title: ':hammer: Member has been banned.',
+                fields: [
+                    ['Moderator', `${message.author.tag} (${message.author.id})`, true],
+                    ['Member', `${member.user.tag} (${member.user.id})`, true],
+                    ['Reason', reason || 'none']
+                ],
+                color: config.colors.done
+            }
         }
     }
 }
