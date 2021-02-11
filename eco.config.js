b800f138f86a545fd2648d25b9293c17368eef05
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-11 11:55:58 +0100

    Create eco.config.js

---
+++ eco.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps : [{
+    name: "LukeBot",
+    script: "./dist/index.js",
+    watch: false,
+    interpreter: "/home/nimplex/.nvm/versions/node/v15.8.0/bin/node"
+  }]
+}
