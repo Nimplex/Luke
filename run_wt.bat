533159627f19201bf609264a763c678c12a505ff
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-13 19:50:13 +0100

    Added echo off

--- run_wt.bat
+++ run_wt.bat
@@ -1,4 +1,11 @@
 :: This script starts windows terminal with 2 powershell
 :: instances in current directory.
 
-wt -d . -p "Windows Powershell" `; split-pane -d . -p "Windows PowerShell" `
+:: Hide input
+@echo off
+
+:: Start Windows Terminal
+wt -d . -p "Windows Powershell" `; split-pane -d . -p "Windows PowerShell"
+
+:: Unhide input
+@echo on
