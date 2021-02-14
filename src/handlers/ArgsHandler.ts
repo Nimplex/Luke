a897f165751ef29edffaea410220f932af302a1b
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-14 18:51:40 +0100

    Update ArgsHandler.ts

--- src/handlers/ArgsHandler.ts
+++ src/handlers/ArgsHandler.ts
@@ -20,9 +20,9 @@ export default async function (args: any[], command: Command, message: message) 
 
         for (let i = 0; i < command.usage.length; i++) {
             if (command.usage[i].type == 'mention' && !message.mentions.members?.first())
-                return error(message, `Wrong ${command.usage[i].required ? `<${command.usage[i].name}>` : `[${command.usage[i].name}]`} argument\n\nIt needs to be ${command.usage[i].required ? `<${command.usage[i].type}>` : `[${command.usage[i].type}]`}.`)
+                return error(message, `Wrong ${command.usage[i].required ? `<${command.usage[i].name}>` : `[${command.usage[i].name}]`} argument\n\nIt needs to be ${command.usage[i].type}.`)
             if (command.usage[i].type == 'number' && isNaN(args[i]))
-                return error(message, `Wrong ${command.usage[i].required ? `<${command.usage[i].name}>` : `[${command.usage[i].name}]`} argument\n\nIt needs to be ${command.usage[i].required ? `<${command.usage[i].type}>` : `[${command.usage[i].type}]`}.`)
+                return error(message, `Wrong ${command.usage[i].required ? `<${command.usage[i].name}>` : `[${command.usage[i].name}]`} argument\n\nIt needs to be ${command.usage[i].type}.`)
             console.log(isNaN(args[i]))
         }
         
