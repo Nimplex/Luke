6f8f2947e681c45d2f286e6c5b4c7d0f4cd87b76
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-15 16:29:00 +0100

    Update router.ts

--- src/dashboard/router.ts
+++ src/dashboard/router.ts
@@ -1,5 +1,11 @@
 import { Application } from 'express'
 
 export = (app: Application) => {
+    // Dashboard
     app.get('/', (req, res) => res.render('index'))
+
+    // API
+    app.post('/api/login', (req, res) => {
+        console.log(req, req.body)
+    })
 }
