9fa2e373b6e99715984e96543bf437e42227b91c
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-15 16:40:50 +0100

    session and login endpoint

--- src/dashboard/router.ts
+++ src/dashboard/router.ts
@@ -3,9 +3,19 @@ import { Application } from 'express'
 export = (app: Application) => {
     // Dashboard
     app.get('/', (req, res) => res.render('index'))
+    app.get('/dashboard', (req, res) => {
+        res.render('dashboard', { body: req.session?.body })
+    })
 
     // API
     app.post('/api/login', (req, res) => {
         console.log(req, req.body)
+        if (req.session) req.session.body = req.body
+        res.redirect('/dashboard')
+    })
+    app.get('/api/logout', (req, res) => {
+        req.session?.destroy(() => {
+            res.redirect('/')
+        })
     })
 }
--- src/dashboard/server.ts
+++ src/dashboard/server.ts
@@ -1,8 +1,10 @@
 import Console from '../modules/Console'
 import express from 'express'
+import session from'express-session'
 import { join } from 'path'
 
 const { server } = require('../../files/config.json')
+const tokens = require('../../files/tokens.json')
 
 export default class Server {
     app: express.Application
@@ -10,9 +12,16 @@ export default class Server {
     constructor() {
         this.app = express()
 
+        this.app.use(session({
+            secret: tokens.session_secret,
+            resave: false,
+            saveUninitialized: true,
+            cookie: { secure: true }
+        }))
         this.app.use(express.json())
         this.app.use(express.urlencoded({ extended: true }))
         this.app.use(express.static(join(__dirname, '..', '..', 'public')))
+        this.app.set('trust proxy', 1)
         this.app.set('views', join(__dirname, '..', '..', 'public', 'views'))
         this.app.set('view engine', 'pug')
 
