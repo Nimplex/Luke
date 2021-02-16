d3a0b0dee55283f48ad21b57672d8e64a6faa9e2
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-16 13:26:16 +0100

    fetching informations from API

--- src/dashboard/router.ts
+++ src/dashboard/router.ts
@@ -1,8 +1,9 @@
 import { Application } from 'express'
-import fetch from 'node-fetch'
 import FormData from 'form-data'
+import fetch from 'node-fetch'
 
 const tokens = require('../../files/tokens.json')
+const { server } = require('../../files/config.json')
 
 export = (app: Application) => {
     // Dashboard
@@ -16,9 +17,47 @@ export = (app: Application) => {
         const code = req.query.code
         if (!code) return res.redirect('/403')
 
-        req.session!['code'] = code
+        req.session!.code = code
 
-        res.redirect('/dashboard')
+        res.redirect('/callback')
+    })
+    app.get('/callback', (req, res) => {
+        const Data = new FormData()
+        Data.append('client_id', server.id || 'NA')
+        Data.append('client_secret', tokens.secret || 'NA')
+        Data.append('grant_type', 'authorization_code')
+        Data.append('code', req.session?.code || 'NA')
+        Data.append('redirect_uri', process.argv[2] ? server.dev_uri : server.redirect_uri)
+        Data.append('scope', 'identify guilds')
+
+        fetch('https://discord.com/api/oauth2/token', {
+            method: 'POST',
+            body: Data
+        }).then(res => res.json()).then(json => {
+            console.log(json)
+            if (!json.access_token) return res.redirect('/400')
+
+            fetch('https://discord.com/api/users/@me', {
+                method: 'GET',
+                headers: {
+                    authorization: `${json.token_type} ${json.access_token}`
+                }
+            }).then(res => res.json()).then(user => {
+                user.tag = `${user.username}#${user.discriminator}`
+                user.avatarURL = user.avatar ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024` : null
+                req.session!.user = user
+
+                fetch('https://discord.com/api/users/@me/guilds', {
+                    method: 'GET',
+                    headers: {
+                        authorization: `${json.token_type} ${json.access_token}`
+                    }
+                }).then(res => res.json()).then(guilds => {
+                    req.session!.guilds = guilds
+                    res.redirect('/dashboard')
+                })
+            })
+        })
     })
     app.get('/api/logout', (req, res) => {
         req.session?.destroy(() => {
