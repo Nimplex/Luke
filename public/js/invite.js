5d786464ff46c4fa5875012e685e72015e7761fb
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-18 13:25:18 +0100

    center invite popup

---
+++ public/js/invite.js
@@ -0,0 +1,13 @@
+const inv = document.getElementsByClassName('inv')
+
+const open = (id) => {
+    const url = `https://discord.com/api/oauth2/authorize?client_id=576478552158109716&disable_guild_select=true&permissions=8&guild_id=${id}&scope=bot`
+    const specs = `location=no,toolbar=no,menubar=no,scrollbars=no,resizable=no,status=no,titlebar=no,height=750,width=500,top=${(window.screen.height - 750) / 2},left=${(window.screen.width - 500) / 2}`
+    const wind = window.open(url, 'invite', specs, false)
+    const inter = setInterval(() => { wind.closed ? location.reload() : null }, 500)
+}
+
+
+for (let i = 0; i < inv.length; i++) {
+    inv[i].addEventListener('click', () => open(inv[i].dataset.id))
+}
