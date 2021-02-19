e107e6b0c8b5a0a7285c8a62b0e130b365bc939b
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-19 13:14:49 +0100

    welcomer

--- src/handlers/EventHandler.ts
+++ src/handlers/EventHandler.ts
@@ -7,32 +7,40 @@ export default class EventHandler {
     constructor(Luke: Luke) {
         Luke.on('ready', () => require('./events/ready')(Luke))
         Luke.on('message', message => require('./events/message')(Luke, message))
-        // Luke.on('guildMemberAdd', async member => {
-        //     const guild = await guildManager.get(member.guild.id)
-        //     if (guild.wenabled == true && guild.welcome_channel && guild.welcome_id && guild.welcome_token) {
-        //         const wClient = new WebhookClient(guild.welcome_id, guild.welcome_token)
-        //         if (!wClient.id || !wClient.token || !wClient.client) return
-        //         else wClient.send(Embed({
-        //             title: 'Welcome',
-        //             description: `Welcome, **${member.user.tag}** to the **${member.guild.name}**`,
-        //             thumbnail: member.user.avatarURL({ dynamic: true }),
-        //             disableDescriptionCodeBlock: true
-        //         }))
-        //     }
-        // })
-        // Luke.on('guildMemberRemove', async member => {
-        //     const guild = await guildManager.get(member.guild.id)
-        //     if (guild.lenabled == true && guild.leave_channel && guild.leave_id && guild.leave_token) {
-        //         const wClient = new WebhookClient(guild.leave_id, guild.leave_token)
-        //         if (!wClient.id || !wClient.token || !wClient.client) return
-        //         else wClient.send(Embed({
-        //             title: 'Goodbye',
-        //             description: `Goodbye **${member.user?.tag}**`,
-        //             thumbnail: member.user?.avatarURL({ dynamic: true }),
-        //             disableDescriptionCodeBlock: true,
-        //             color: '#eb4034'
-        //         }))
-        //     }
-        // })
+        Luke.on('guildMemberAdd', async member => {
+            const guild = await guildManager.get(member.guild.id)
+            if (guild.welcomer.welcome.enabled == true && guild.welcomer.welcome.channel) {
+                const wClient = new WebhookClient(guild.welcomer.welcome.channel.webhook.id, guild.welcomer.welcome.channel.webhook.token)
+                if (!wClient.id || !wClient.token || !wClient.client) return
+                else {
+                    let message = guild.welcomer.welcome.message
+                    if (guild.welcomer.welcome.random_message.enabled) message = guild.welcomer.welcome.random_message.messages[Math.floor(Math.random()*guild.welcomer.welcome.random_message.messages.length)]
+                    wClient.send(Embed({
+                        title: 'Welcome',
+                        description: message.replace(/{user.name}/gm, member.user?.tag || 'X').replace(/{guild.name}/gm, member.guild.name || 'X'),
+                        thumbnail: member.user.avatarURL({ dynamic: true }),
+                        disableDescriptionCodeBlock: true
+                    }))
+                }
+            }
+        })
+        Luke.on('guildMemberRemove', async member => {
+            const guild = await guildManager.get(member.guild.id)
+            if (guild.welcomer.goodbye.enabled == true && guild.welcomer.goodbye.channel) {
+                const lClient = new WebhookClient(guild.welcomer.goodbye.channel.webhook.id, guild.welcomer.goodbye.channel.webhook.token)
+                if (!lClient.id || !lClient.token || !lClient.client) return
+                else {
+                    let message = guild.welcomer.goodbye.message
+                    if (guild.welcomer.goodbye.random_message.enabled) message = guild.welcomer.goodbye.random_message.messages[Math.floor(Math.random()*guild.welcomer.goodbye.random_message.messages.length)]
+                    lClient.send(Embed({
+                        title: 'Goodbye',
+                        description: message.replace(/{user.name}/gm, member.user?.tag || 'X').replace(/{guild.name}/gm, member.guild.name || 'X'),
+                        thumbnail: member.user?.avatarURL({ dynamic: true }),
+                        disableDescriptionCodeBlock: true,
+                        color: '#eb4034'
+                    }))
+                }
+            }
+        })
     }
 }
