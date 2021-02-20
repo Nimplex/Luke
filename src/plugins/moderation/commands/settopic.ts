1c9a7206753489b246d9dd28fce8a260dcc2d213
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-02-20 23:01:38 +0100

    settopic

---
+++ src/plugins/moderation/commands/settopic.ts
@@ -0,0 +1,31 @@
+import { Command } from '@/types'
+
+const colors = require('../../../../files/colors.json')
+
+const command: Command = {
+    triggers: ['settopic'],
+    description: 'Changes channel topic.',
+    permissions: {
+        bot: ['MANAGE_CHANNELS'],
+        user: ['MANAGE_CHANNELS'],
+    },
+    usage: [
+        {
+            name: 'topic',
+            type: 'text',
+        }
+    ],
+    execute: async(message, Luke, ...args) => {
+        let channel = message.mentions.channels.first() || message.channel
+        let topic = args.join(' ') || ''
+
+        await channel.setTopic(topic)
+
+        Luke.embed({
+            object: message,
+            description: `Channel topic changed to:\n${topic || 'none'}`,
+        })
+    }
+}
+
+module.exports = command
