223d4aae6fb853c3d1ec92e184f73000a41b18b3
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-03-07 17:52:35 +0100

    lol

--- src/plugins/levels/commands/level.ts
+++ src/plugins/levels/commands/level.ts
@@ -14,12 +14,12 @@ const command: Command = {
         const member = message.mentions.members?.first()?.user || message.author
         const user = await Luke.LevelManager.get(member.id)
 
-        const xp = user.experience.toString().split('.')
+        const xp = Object.assign({0: '', 1: '0'}, user.experience.toString().split('.'))
 
         Luke.embed({
             object: message,
             title: `${member.username}'s stats`,
-            description: `${user.level} Level\n${xp[0]}${xp[1] ? `.${xp[1]?.substr(0, 2)}`: ''} XP\n${user.messagesCount} Messages`
+            description: `${user.level} Level\n${xp[0]}.${xp[1].substr(0, 2)} XP\n${user.messagesCount} Messages`
         })
     }
 }
