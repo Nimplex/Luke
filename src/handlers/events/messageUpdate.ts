413fa69f66dca3384f089d4a610c01260e275751
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-03-08 11:31:59 +0100

    automoderator

---
+++ src/handlers/events/messageUpdate.ts
@@ -0,0 +1,18 @@
+import guildManager from '@/database/guildManager'
+import { Luke } from '@/index'
+import { message } from '@/types'
+
+module.exports = async (Luke: Luke, message: message) => {
+    if (message.author.bot || !message.guild) return
+
+    const guild = await guildManager.get(message.guild.id)
+
+    if ((!message.member?.permissions.has('MANAGE_MESSAGES') && guild.automoderator.spam) && message.mentions.members && message.mentions.members?.size >= 3) {
+        message.delete()
+        message.reply('Don\'t spam!')
+    }
+    if ((!message.member?.permissions.has('MANAGE_MESSAGES') && guild.automoderator.invites) && message.content.includes('discord.gg')) {
+        message.delete()
+        message.reply('Don\'t send invites!')
+    }
+}
