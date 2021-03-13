d4d89508c2357ca226745a5358c0fcffe97f1042
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-03-13 13:53:20 +0100

    guild -> server

--- src/handlers/events/message.ts
+++ src/handlers/events/message.ts
@@ -36,9 +36,9 @@ module.exports = async (Luke: Luke, message: message) => {
         message.reply('Don\'t spam!')
     }
     guild.automoderator?.blacklist.forEach(word => {
-        if (message.content.includes(word)) {
+        if (message.content.includes(word) || message.content.includes(Buffer.from(word).toString('base64'))) {
             message.delete()
-            message.reply('This word isn\'t allowed on this guild!')
+            message.reply('This word isn\'t allowed on this server!')
         }
     })
     if (!message.member?.permissions.has('MANAGE_MESSAGES') && guild.automoderator?.invites) {
