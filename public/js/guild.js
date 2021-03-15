c7a478572f82be148ca9e9b1b8fd2c03957fc8b5
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-03-15 21:35:31 +0100

    deleted old dashboard

--- public/js/guild.js
+++
@@ -1,7 +0,0 @@
-const navbar_options = document.getElementsByClassName('option')
-
-Object.values(navbar_options).forEach(elem => {
-    elem.addEventListener('click', () => {
-        location.href = `/dashboard/${data.dataset.id}/${elem.dataset.id}`
-    })
-})
