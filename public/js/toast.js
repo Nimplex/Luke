da6dd3afc7c5f9f830bc3d8db1fb2500263ea3e3
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-03-16 16:33:09 +0100

    cookies alert

---
+++ public/js/toast.js
@@ -0,0 +1,12 @@
+const toastElem = document.getElementById('liveToast')
+const closeCookies = document.getElementById('closeCookies')
+
+window.onload = () => {
+    const toast = new bootstrap.Toast(toastElem, { animation: true, autohide: false })
+    const toastClosed = localStorage.getItem('cookiesAlert')
+    if (!toastClosed) toast.show()
+}
+
+closeCookies.addEventListener('click', () => {
+    localStorage.setItem('cookiesAlert', true)
+})
