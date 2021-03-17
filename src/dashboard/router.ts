1b08c7208d972c1a1de43c109d0b1be4ba5bb854
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-03-17 13:02:52 +0100

    new dashboard style

--- src/dashboard/router.ts
+++ src/dashboard/router.ts
@@ -46,23 +46,7 @@ export = (app: Application) => {
 
         switch (cat) {
             case 'basic':
-                res.render('basic', {
-                    guild: guild,
-                    user: req.session.user,
-                    data: server,
-                    channels: channels
-                })
-                break;
-            case 'welcomer':
-                res.render('welcomer', {
-                    guild: guild,
-                    user: req.session.user,
-                    data: server,
-                    channels: channels
-                })
-                break;
-            case 'automod':
-                res.render('automod', {
+                res.render('guild', {
                     guild: guild,
                     user: req.session.user,
                     data: server,
@@ -70,12 +54,7 @@ export = (app: Application) => {
                 })
                 break;
             default:
-                res.render('basic', {
-                    guild: guild,
-                    user: req.session.user,
-                    data: server,
-                    channels: channels
-                })
+                res.redirect(`/${id}/basic`)
                 break;
         }
     })
@@ -204,5 +183,5 @@ export = (app: Application) => {
     // Errors
     app.get('/401', (req, res) => res.status(401).render('401'))
     app.get('/404', (req, res) => res.status(404).render('404'))
-    app.get('/*', (req, res) => res.redirect('/404'))
+    // app.get('/*', (req, res) => res.redirect('/404'))
 }
