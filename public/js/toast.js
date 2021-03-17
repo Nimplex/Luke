61fe7469df7561517affb2574a69a33a95bf045b
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-03-17 22:33:47 +0100

    small changes

--- public/js/toast.js
+++ public/js/toast.js
@@ -1,4 +1,5 @@
 const toastElem = document.getElementById('liveToast')
+const toastBody = document.getElementsByClassName('toast-body')[0]
 const closeCookies = document.getElementById('closeCookies')
 
 window.onload = () => {
@@ -10,3 +11,11 @@ window.onload = () => {
 closeCookies.addEventListener('click', () => {
     localStorage.setItem('cookiesAlert', true)
 })
+
+const alert = (type = 'done', message = 'This website uses cookies. We use cookies to ensure that we give you the best experience on our website.', autohide = true) => {
+    const toast = new bootstrap.Toast(toastElem, { animation: true, autohide: autohide })
+    toastBody.innerHTML = message
+    toastElem.setAttribute('aria-alive', type == 'error' ? 'assertive' : 'polite')
+    toastElem.setAttribute('role', type == 'error' ? 'alert' : 'status')
+    toast.show()
+}
