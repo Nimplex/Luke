f3be877a34188428d03bf196d764b7c823d0413d
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-03-19 09:17:47 +0100

    three powershell instances

--- run_wt.bat
+++ run_wt.bat
@@ -1,11 +1,15 @@
-:: This script starts windows terminal with 2 powershell
+:: This script starts windows terminal with 3 powershell
 :: instances in current directory.
 
 :: Hide input
 @echo off
 
+:: sass .\public\scss\:.\public\css --watch
+:: tsc --watch
+:: nodemon --ignore public . dev
+
 :: Start Windows Terminal
-wt -d . -p "Windows Powershell" `; split-pane -d . -p "Windows PowerShell"
+wt -d . -p "Windows Powershell" `; split-pane -d . -p "Windows PowerShell" `; split-pane -H -d . -p "Windows PowerShell"
 
 :: Unhide input
 @echo on
