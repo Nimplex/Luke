81ef7c45fe1f425bd6db0a6f49d70e5e03166737
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-03-24 11:24:44 +0100

    remove unused function
    - Removed alert function

--- public/js/toast.js
+++ public/js/toast.js
@@ -14,21 +14,3 @@ window.onload = () => {
 closeCookies.addEventListener('click', () => {
     localStorage.setItem('cookiesAlert', true)
 })
-
-const alert = (
-    type = 'done',
-    message = 'This website uses cookies. We use cookies to ensure that we give you the best experience on our website.',
-    autohide = true
-) => {
-    const toast = new bootstrap.Toast(toastElem, {
-        animation: true,
-        autohide: autohide,
-    })
-    toastBody.innerHTML = message
-    toastElem.setAttribute(
-        'aria-alive',
-        type == 'error' ? 'assertive' : 'polite'
-    )
-    toastElem.setAttribute('role', type == 'error' ? 'alert' : 'status')
-    toast.show()
-}
