39664845e041601f314394335eece7c774c3978f
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-03-24 08:32:23 +0100

    Automoderator update, easier leveling
    - Increased ammount of XP that user earns.
    - Automoderator now detects role pings.
    - Formated code.
    - Added .prettierrc.
    - MessageUpdate detects base64.

--- src/handlers/events/message.ts
+++ src/handlers/events/message.ts
@@ -1,25 +1,30 @@
-import { Luke } from '@/index'
-import { Command, message } from '@/types'
-import ArgsHandler from '../ArgsHandler'
-import guildManager from '../../database/guildManager'
+import { Luke } from "@/index"
+import { Command, message } from "@/types"
+import ArgsHandler from "../ArgsHandler"
+import guildManager from "../../database/guildManager"
 
-const colors = require('../../../files/colors.json')
-const { bot } = require('../../../files/config.json')
+const colors = require("../../../files/colors.json")
+const { bot } = require("../../../files/config.json")
 
-export function error(name: string, message: message, Luke: Luke, command: Command) {
-    switch(name) {
-        case 'permissions_user':
+export function error(
+    name: string,
+    message: message,
+    Luke: Luke,
+    command: Command
+) {
+    switch (name) {
+        case "permissions_user":
             Luke.embed({
                 object: message,
                 description: `You're missing permissions:\n${command.permissions?.user}`,
-                color: colors.error
+                color: colors.error,
             })
             break
-        case 'permissions_bot':
+        case "permissions_bot":
             Luke.embed({
                 object: message,
                 description: `I'm missing permissions:\n${command.permissions?.bot}`,
-                color: colors.error
+                color: colors.error,
             })
             break
     }
@@ -28,45 +33,61 @@ export function error(name: string, message: message, Luke: Luke, command: Comma
 module.exports = async (Luke: Luke, message: message) => {
     if (message.author.bot || !message.guild) return
 
-    Luke.LevelManager.increase(message.author.id, message.content.length / 15, message)
+    Luke.LevelManager.increase(
+        message.author.id,
+        message.content.length / 10,
+        message
+    )
     const guild = await guildManager.get(message.guild.id)
 
-    if ((!message.member?.permissions.has('MANAGE_MESSAGES') && guild.automoderator?.spam) && message.mentions.members && message.mentions.members?.size >= 3) {
+    if (
+        !message.member?.permissions.has("MANAGE_MESSAGES") &&
+        guild.automoderator?.spam &&
+        ((message.mentions.members && message.mentions.members?.size > 3) ||
+            (message.mentions.roles && message.mentions.roles?.size > 2))
+    ) {
         message.delete()
-        message.reply('Don\'t spam!')
+        message.reply("Don't spam!")
     }
-    guild.automoderator?.blacklist.forEach(word => {
-        if (word == '') return
-        if (message.content.includes(word) || message.content.includes(Buffer.from(word).toString('base64'))) {
+    guild.automoderator?.blacklist.forEach((word) => {
+        if (word == "") return
+        if (
+            message.content.includes(word) ||
+            message.content.includes(Buffer.from(word).toString("base64"))
+        ) {
             message.delete()
-            message.reply('This word isn\'t allowed on this server!')
+            message.reply("This word isn't allowed on this server!")
         }
     })
-    if (!message.member?.permissions.has('MANAGE_MESSAGES') && guild.automoderator?.invites) {
-        let invite = /(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?/i;
+    if (
+        !message.member?.permissions.has("MANAGE_MESSAGES") &&
+        guild.automoderator?.invites
+    ) {
+        let invite = /(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?/i
         if (invite.test(message.content)) {
             message.delete()
-            message.reply('Don\'t send invites!')
+            message.reply("Don't send invites!")
         }
     }
 
     if (!message.content.startsWith(guild.prefix || bot.prefix)) return
-    
-    const [commandName, ...args] = message.content.slice(guild.prefix.length || bot.prefix).split(/ +/g)
+
+    const [commandName, ...args] = message.content
+        .slice(guild.prefix.length || bot.prefix)
+        .split(/ +/g)
     const command = Luke.CommandHandler.get(commandName)
 
     if (!command) return
     if (command.dev && !bot.developers.includes(message.author.id)) return
-    if (!message.member?.permissions.has(command.permissions?.user || [])) return error('permissions_user', message, Luke, command)
-    if (!message.guild.me?.permissions.has(command.permissions?.bot || [])) return error('permissions_bot', message, Luke, command)
+    if (!message.member?.permissions.has(command.permissions?.user || []))
+        return error("permissions_user", message, Luke, command)
+    if (!message.guild.me?.permissions.has(command.permissions?.bot || []))
+        return error("permissions_bot", message, Luke, command)
 
     const testArgs = await ArgsHandler(args, command, message)
 
-    if (
-        command?.nsfw &&
-        message.channel.nsfw
-    ) return
-    else if (!testArgs){
+    if (command?.nsfw && message.channel.nsfw) return
+    else if (!testArgs) {
         try {
             const output = await command?.execute(message, Luke, ...args)
             if (output == false) {
--- src/handlers/events/messageUpdate.ts
+++ src/handlers/events/messageUpdate.ts
@@ -1,28 +1,39 @@
-import guildManager from '../../database/guildManager'
-import { Luke } from '@/index'
-import { message } from '@/types'
+import guildManager from "../../database/guildManager"
+import { Luke } from "@/index"
+import { message } from "@/types"
 
 module.exports = async (Luke: Luke, message: message) => {
     if (message.author.bot || !message.guild) return
 
     const guild = await guildManager.get(message.guild.id)
 
-    if ((!message.member?.permissions.has('MANAGE_MESSAGES') && guild.automoderator?.spam) && message.mentions.members && message.mentions.members?.size >= 3) {
+    if (
+        !message.member?.permissions.has("MANAGE_MESSAGES") &&
+        guild.automoderator?.spam &&
+        ((message.mentions.members && message.mentions.members?.size > 3) ||
+            (message.mentions.roles && message.mentions.roles?.size > 2))
+    ) {
         message.delete()
-        message.reply('Don\'t spam!')
+        message.reply("Don't spam!")
     }
-    guild.automoderator?.blacklist.forEach(word => {
-        if (word == '') return
-        if (message.content.includes(word)) {
+    guild.automoderator?.blacklist.forEach((word) => {
+        if (word == "") return
+        if (
+            message.content.includes(word) ||
+            message.content.includes(Buffer.from(word).toString("base64"))
+        ) {
             message.delete()
-            message.reply('This word isn\'t allowed on this guild!')
+            message.reply("This word isn't allowed on this guild!")
         }
     })
-    if (!message.member?.permissions.has('MANAGE_MESSAGES') && guild.automoderator?.invites) {
-        let invite = /(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?/i;
+    if (
+        !message.member?.permissions.has("MANAGE_MESSAGES") &&
+        guild.automoderator?.invites
+    ) {
+        let invite = /(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?/i
         if (invite.test(message.content)) {
             message.delete()
-            message.reply('Don\'t send invites!')
+            message.reply("Don't send invites!")
         }
     }
 }
