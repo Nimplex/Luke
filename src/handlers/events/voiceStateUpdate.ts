838992760246451b5b21d20f3ce254b1835e2da2
Nimplex <39964594+Nimplex@users.noreply.github.com>
2021-04-07 08:53:49 +0200

    remove unused import

--- src/handlers/events/voiceStateUpdate.ts
+++ src/handlers/events/voiceStateUpdate.ts
@@ -1,8 +1,6 @@
 import { Luke } from '@/index'
 import { VoiceState } from 'discord.js'
 
-const colors = require('../../../files/colors.json')
-
 module.exports = async (Luke: Luke, oldState: VoiceState, newState: VoiceState) => {
     if (oldState?.channelID !== newState?.channelID) {
         if (oldState.member?.id === Luke.user?.id) {
